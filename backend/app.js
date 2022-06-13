7023ccb377c9bd0818d6ad7fc3adf43322c81bc4
Nikita Nikiforov <nikifor38@ya.ru>
2022-06-13 18:15:40 +0700

    Bugs Fixed

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(requestLogger);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://mesto.nikiforovnd.nomoredomains.xyz');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
